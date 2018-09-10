f12e925eb270a8bf8357a65a28b5c78b0d6c2441
Szymon Smenda <szymon.smenda@gmail.com>
2018-09-10 12:12:41 +0200

    update

---
+++ src/CompositionChallenge/Bed.java
@@ -0,0 +1,41 @@
+package CompositionChallenge;
+
+public class Bed{
+    private String style;
+    private int pillows;
+    private int height;
+    private int sheets;
+    private int quilt;
+
+    public Bed(String style, int pillows, int height, int sheets, int quilt) {
+        this.height = height;
+        this.pillows = pillows;
+        this.sheets = sheets;
+        this.style = style;
+        this.quilt = quilt;
+    }
+
+    public void make() {
+        System.out.println("Bed making");
+    }
+
+    public String getStyle() {
+        return style;
+    }
+
+    public int getPillows() {
+        return pillows;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getSheets() {
+        return sheets;
+    }
+
+    public int getQuilt() {
+        return quilt;
+    }
+}
---
+++ src/CompositionChallenge/main.java
@@ -0,0 +1,24 @@
+package CompositionChallenge;
+
+public class main{
+
+    public static void main (String[] args){
+        Wall wall = new Wall("West");
+        Wall wal2 = new Wall("East");
+        Wall wal3 = new Wall("South");
+        Wall wal4 = new Wall("North");
+
+        Ceiling ceiling = new Ceiling(12,55);
+
+        Bed bed = new Bed("Style", 4,3,3,1);
+
+        Lamp lamp = new Lamp("Clasic", false, 100);
+
+        Bedroom bedRoom = new Bedroom("Szymon" ,wall,wal2,wal3,wal4, ceiling,bed,lamp);
+
+        bedRoom.makeBed();
+        bedRoom.getLamp().turnOn();
+    }
+
+
+}
