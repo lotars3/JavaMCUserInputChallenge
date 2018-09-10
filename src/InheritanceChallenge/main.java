05cba649492ee15bb76f0ce9bf58a834c09b8db4
Szymon Smenda <szymon.smenda@gmail.com>
2018-09-10 11:09:28 +0200

    update

---
+++ src/InheritanceChallenge/main.java
@@ -0,0 +1,12 @@
+package InheritanceChallenge;
+
+public class main{
+
+    public static void main(String[] args){
+      Porshe porshe = new Porshe(20);
+
+      porshe.accelerate(30);
+      porshe.steer(45);
+
+    }
+}
