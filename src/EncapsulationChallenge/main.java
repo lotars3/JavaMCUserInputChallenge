15e921c5420a895b8e724ebfb8d73331ab21e74b
Szymon Smenda <szymon.smenda@gmail.com>
2018-09-10 14:17:00 +0200

    update

---
+++ src/EncapsulationChallenge/main.java
@@ -0,0 +1,15 @@
+package EncapsulationChallenge;
+
+public class main{
+
+    public static void main(String[] args) {
+        Printer printer = new Printer(50, true);
+
+        System.out.println("Initial page count" + printer.getPagesPrinted());
+        int pagesPrinted = printer.print(4);
+        System.out.println("Pages printed was" + pagesPrinted + " new total print count for printed=" + printer.getPagesPrinted());
+        pagesPrinted = printer.print(2);
+        System.out.println("Pages printed was" + pagesPrinted + " new total print count for printed=" + printer.getPagesPrinted());
+
+    }
+}
